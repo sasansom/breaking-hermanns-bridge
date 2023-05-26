220a26c4566afb96d5dbb933d93a156205a74835
David Fifield <david@bamsoftware.com>
2023-05-26 12:48:41 -0600

    Add caesura_word_n to hermann-filter.py output.
    Also output all words in a line that end at sedes 8.5, not just the
    single word that immediately precedes the first word that starts at
    sedes 8.5.

--- hermann-filter.py
+++ hermann-filter.py
@@ -4,7 +4,8 @@
 #   filter-hermann.py sedes/corpus/*.csv
 #
 # Filters the input to include only the rows with a word at sedes 8.5, and the
-# rows of the preceding sedes.
+# rows of the words at the preceding sedes. Adds a caesura_word_n column
+# containing the word_n of the first word at the caesura in the line.
 
 import csv
 import getopt
@@ -16,7 +17,9 @@ def usage(file = sys.stdout):
 Usage: {sys.argv[0]} INPUT.CSV...
 
 Filters the input to include only the rows with a word at sedes 8.5, and
-the rows of the preceding sedes.
+the rows of the words at the preceding sedes. Adds a caesura_word_n
+column containing the word_n of the first word at the caesura in the
+line.
 """, end="", file=file)
 
 def normalize(s):
@@ -54,7 +57,9 @@ def metrical_length(metrical_shape):
     return sum({"⏑": 0.5, "–": 1.0}[c] for c in metrical_shape)
 
 def process(r, w):
-    before_row = None
+    prev_loc = None
+    pre_buffer = []
+    caesura_word_n = None
     for row in r:
         try:
             sedes_begin = float(row["sedes"])
@@ -62,14 +67,44 @@ def process(r, w):
             print(f"missing sedes: {','.join(row.values())!r}", file = sys.stderr)
             continue
         sedes_end = sedes_begin + metrical_length(row["metrical_shape"])
-        if sedes_end == 8.5:
-            # Word before the caesura.
-            before_row = row  # Will output this line.
-        elif sedes_begin == 8.5:
-            # Word after the caesura.
-            if before_row is not None:
-                w.writerow(before_row)
-                w.writerow(row)
+
+        # Whenever we advance to a new line, output any previous buffer of words
+        # that end at sedes 8.5.
+        loc = (row["work"], row["book_n"], row["line_n"])
+        if prev_loc is None or prev_loc != loc:
+            prev_loc = loc
+            caesura_word_n = None
+            # We don't actually expect pre_buffer to ever be non-empty here,
+            # because it would usually have been output and emptied by the code
+            # below that handles words that are at 8.5. But it could occur if a
+            # line were fragmentary and ended exactly at 8.5.
+            w.writerows(dict(r, **{"caesura_word_n": caesura_word_n}) for r in pre_buffer)
+            pre_buffer.clear()
+
+        # Check if the current word begins at 8.5 before checking if it ends at
+        # 8.5. This only matters with words whose metrical length is zero, like
+        # "δ’". Prioritizing the "begin" check means that such words will be
+        # considered to come after the caesura. It would also be a valid
+        # interpretation to put them before the caesura, but such cases are most
+        # commonly elision that should attach to the word to the right.
+        if sedes_begin == 8.5:
+            # If this is the first word at 8.5 in this line, mark its word_n as
+            # the caesura_word_n, and output the buffer of words that end at
+            # 8.5.
+            if caesura_word_n is None:
+                caesura_word_n = row["word_n"]
+                w.writerows(dict(r, **{"caesura_word_n": caesura_word_n}) for r in pre_buffer)
+                pre_buffer.clear()
+            w.writerow(dict(row, **{"caesura_word_n": caesura_word_n}))
+        elif sedes_end == 8.5:
+            # A word at the sedes immediately before the caesura. We will output
+            # a row for this word, but we don't yet know what its caesura_word_n
+            # should be. Buffer it until we either see a word at 8.5, or enter a
+            # different line.
+            pre_buffer.append(row)
+    caesura_word_n = None
+    w.writerows(dict(r, **{"caesura_word_n": caesura_word_n}) for r in pre_buffer)
+    pre_buffer.clear()
 
 opts, args = getopt.gnu_getopt(sys.argv[1:], "h", ("help",))
 for o, _ in opts:
@@ -78,6 +113,6 @@ for o, _ in opts:
         sys.exit(0)
 
 r = input_dictreader(args)
-w = csv.DictWriter(sys.stdout, fieldnames = r.fieldnames, lineterminator = "\n")
+w = csv.DictWriter(sys.stdout, fieldnames = r.fieldnames + ["caesura_word_n"], lineterminator = "\n")
 w.writeheader()
 process(r, w)
