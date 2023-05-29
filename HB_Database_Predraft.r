1c76ed7053b7bfc0d9b0958a0afb3568bf2bc7a9
David Fifield <david@bamsoftware.com>
2023-05-29 14:05:12 -0600

    Quit making the book_n factor globally unique.
    Was working on this for the chart of breaks clustering, but it's not
    needed or desirable for the rest.

--- HB_Database_Predraft.r
+++ HB_Database_Predraft.r
@@ -35,8 +35,7 @@ data <- read_csv(
 ) %>%
 	mutate(
 		across(c(breaks_hb_schein, is_speech), ~ recode(.x, "Yes" = TRUE, "No" = FALSE)),
-		enclitic = recode(enclitic, "Enclitic" = TRUE, `Non-enclitic` = FALSE),
-		book_n = as_factor(sprintf("%s%s", work, book_n))
+		enclitic = recode(enclitic, "Enclitic" = TRUE, `Non-enclitic` = FALSE)
 	)
 
 # Sanity check that some columns that are supposed to be constant within a line
